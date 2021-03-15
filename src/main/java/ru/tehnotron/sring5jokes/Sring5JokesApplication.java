9f58be9aa043af0d161653db331b487023f45c07
mgbardakov <mgbardakov@gmail.com>
2021-03-15 12:40:27 +0300

    initial project

---
+++ src/main/java/ru/tehnotron/sring5jokes/Sring5JokesApplication.java
@@ -0,0 +1,13 @@
+package ru.tehnotron.sring5jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sring5JokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sring5JokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tehnotron/sring5jokes/Sring5JokesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tehnotron.sring5jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sring5JokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
